59a6eda24781032c303cbfbc35627db95093535d
TURISMO-M <abzmahfuzgamingofficial@gmail.com>
2026-09-07 12:03:44 +0600

    Replace CONSOLE_Init with CON_Init in main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -8,9 +8,12 @@ static GXRModeObj *rmode = NULL;
 int main(int argc, char **argv) {
     VIDEO_Init();
     PAD_Init();
+    
     rmode = VIDEO_GetPreferredMode(NULL);
     frameBuffer = MEM_K0_TO_K1(SYS_AllocateFramebuffer(rmode));
-    CONSOLE_Init(frameBuffer, 20, 20, rmode->fbWidth, rmode->xfbHeight, rmode->fbWidth * VI_DISPLAY_PIX_SZ);
+    
+    CON_Init(frameBuffer, 20, 20, rmode->fbWidth, rmode->xfbHeight, rmode->fbWidth * VI_DISPLAY_PIX_SZ);
+    
     VIDEO_Configure(rmode);
     VIDEO_SetNextFramebuffer(frameBuffer);
     VIDEO_SetBlack(FALSE);
